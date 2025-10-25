26303dec6c55a0462c23b8f3d660b71f84c22316
TarunZach <tzacharias.97@gmail.com>
2025-10-25 14:44:55 +0200

    add provider

---
+++ app/provider.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { ProviderProps } from "@/lib/types";
+
+export default function Provider({ children }: ProviderProps) {
+  return <div>{children}</div>;
+}
--- config/db.ts
+++ config/db.ts
@@ -1,9 +1,5 @@
+import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-const dbUrl = process.env.DATABASE_URL;
-
-if (!dbUrl) {
-  throw new Error("DATABASE_URL is not defined in environment variables.");
-}
-
-export const db = drizzle(dbUrl);
+const pgsql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: pgsql });
